fc70ded2317b2c16f7a3f57e2ca9e5e28860a384
carmenlm <carmenlopezmedina@yahoo.es>
2016-05-08 23:13:44 +0200

    actualizacion

--- js/ruta.js
+++ js/ruta.js
@@ -1,14 +1,20 @@
 /**
- * Created by Carmen on 02/05/2016.
+ * Created by Carmen on 28/04/2016.
  */
+
+//TODO marcar primer restaurante
+//TODO sacar info window, datos de restaurante
 
 
 var directionDisplay;
 var directionsService = new google.maps.DirectionsService();
 var map;
-var gmarkers = new Array();
 var places = new Array();
 
+
+/**
+ * funcion iniciar mapa
+ */
 function initialize() {
     directionsDisplay = new google.maps.DirectionsRenderer();
     var centro = new google.maps.LatLng(36.510071, -4.882447);
@@ -17,39 +23,62 @@ function initialize() {
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         center: centro
     }
+
+    //creo el mapa
     map = new google.maps.Map(document.getElementById("map"), myOptions);
     directionsDisplay.setMap(map);
 
+    var contenido = "hola";
+
+    infowindow = new google.maps.InfoWindow({
+        content: contenido
+    });
+
+    //boton
     var btnEnviar = document.getElementById('btnEnviar');
 
+    //evento click del boton
     btnEnviar.onclick = function () {
 
+        //añado la direccion al array places
         var address = document.getElementById('address').value;
 
         places.push(address);
 
+        //y calculo la ruta
         calcRoute();
 
 
     };
+
+
 }
 
 function calcRoute() {
 
 
+    //variables
+    //array con paradas intermedias
     var paradas = [];
+    //primer lugar
     var origen = places[0];
     var destino;
 
 
+    //si tengo mas de dos lugares
     if (places.length >= 2) {
+        //si tengo un lugar
         if (places.length == 1) {
             console.log('nada');
+
         } else {
+            //si tengo más lugares
             for (var i = 1; i < places.length - 1; i++) {
 
                 var obj = places[i];
 
+                //añado los lugares intermedios
+                //salto el primero y el ultimo
                 paradas.push({
                     location: obj,
                     stopover: true
@@ -58,8 +87,9 @@ function calcRoute() {
         }
 
 
+        //
         destino = places[places.length - 1];
-
+        //creo un objeto request para la peticion de ruta
         var request = {
             origin: origen,
             destination: destino,
@@ -67,19 +97,35 @@ function calcRoute() {
             optimizeWaypoints: true,
             travelMode: google.maps.DirectionsTravelMode.WALKING
         };
+        //creo la peticion de ruta
         directionsService.route(request, function (response, status) {
             if (status == google.maps.DirectionsStatus.OK) {
                 directionsDisplay.setDirections(response);
                 var route = response.routes[0];
+                console.log(route);
+
             } else {
                 alert("directions response " + status);
             }
         });
     } else {
-        destino = null;
+        console.log('nada');
 
     }
 
+
+}
+
+function showInfo(response, markers, info, map) {
+
+    var myRoute = response.routes[0].legs[0];
+
+    for (var i = 0; i < myRoute.steps.length; i++) {
+        var marcador = markers[i] || new google.maps.Marker;
+        marcador.setMap(map);
+        marcador.setPosition();
+
+    }
 
 }
 
---
+++ js/rutaFinal.js
@@ -0,0 +1,205 @@
+/**
+ * Created by Carmen on 08/05/2016.
+ */
+var places = [];
+var directionsService = new google.maps.DirectionsService();
+var directionsDisplay;
+var mapa;
+
+/**
+ * funcion iniciar mapa
+ */
+function initialize() {
+
+    var markers = [];
+
+
+    var centro = new google.maps.LatLng(36.510071, -4.882447);
+    var myOptions = {
+        zoom: 6,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        center: centro
+    }
+
+
+    //creo el mapa por defecto
+    mapa = new google.maps.Map(document.getElementById("map"), myOptions);
+
+
+    //creo renderizador para las direcciones y lo asocio al mapa
+    directionsDisplay = new google.maps.DirectionsRenderer(
+        {
+            map: mapa,
+            //aqui quito los marcadores que se crean por defecto
+            suppressMarkers: true
+        }
+    );
+
+
+    //instancio un infowindow para cada paso de la ruta
+    var infoWindow = new google.maps.InfoWindow;
+
+
+    //boton
+    var btnEnviar = document.getElementById('btnEnviar');
+
+    //evento click del boton
+    btnEnviar.onclick = function () {
+
+        //capturo la informacion del restaurante y de la tapa
+        var address = document.getElementById('address').value;
+        var name = document.getElementById('name').value;
+        var phone = document.getElementById('phone').value;
+        var email = document.getElementById('email').value;
+
+        //creo un objeto restaurante
+        var restaurante = {
+            nombre: name,
+            telefono: phone,
+            email: email,
+            direccion: address
+        }
+
+        //añado el objeto al array
+        places.push(restaurante);
+
+        // console.log(places);
+        //y calculo la ruta
+        calcRoute(directionsDisplay, directionsService, markers, infoWindow, mapa);
+
+
+    };
+
+
+}
+/**
+ * funcion que va a calcular la ruta entre los puntos señalados y mostrar en cada
+ * info de cada restaurante
+ * @param directionsDisplay --renderizador del mapa
+ * @param directionsService --servicio de indicaciones
+ * @param markers -- marcadores de la ruta
+ * @param stepDisplay --infowindows de cada punto
+ * @param map --mapa donde se muestra la ruta
+ */
+function calcRoute(directionsDisplay, directionsService, markers, stepDisplay, map) {
+
+    //reset de los marcadores existentes
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+    }
+
+
+    //variables
+    //array con paradas intermedias
+    var paradas = [];
+    //primer lugar
+    var origen = places[0].direccion;
+    var destino;
+    console.log(origen);
+
+    //recorro el array de restaurantes
+    for (var i = 1; i < places.length; i++) {
+
+        //añado los restaurante intermedios entre el origen y el destino a los waypoints
+        var lugar = places[i].direccion;
+
+        //añado los lugares intermedios
+        //salto el primero y el ultimo
+        paradas.push({
+            location: lugar,
+            stopover: true
+        });
+    }
+
+    destino = places[places.length - 1].direccion;
+    //creo un objeto request para la peticion de ruta
+    var request = {
+        origin: origen,
+        destination: destino,
+        waypoints: paradas,
+        optimizeWaypoints: true,
+        travelMode: google.maps.DirectionsTravelMode.WALKING
+    };
+    //creo la peticion de ruta
+    directionsService.route(request, function (response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+
+            //muestro los marcadores con el metododo
+            mostrarPasos(response, markers, stepDisplay, map);
+            //señalo las direcciones
+            directionsDisplay.setDirections(response);
+
+        } else {
+            alert("directions response " + status);
+        }
+    });
+
+
+}
+
+/**
+ * funcion que va a asociar un marcador personalizado a cada punto de la ruta
+ * @param response -- respuesta de la peticion al servicio
+ * @param markers --array marcadores
+ * @param stepDisplay -- infowindow
+ * @param map --mapa donde se muestra la ruta
+ */
+function mostrarPasos(response, markers, stepDisplay, map) {
+
+
+    //por cada paso, asocio un marcador personalizado
+    //añado el marcador al array
+
+    var myRoute = response.routes[0];
+
+    //recorro los distintos legs de la ruta
+    for (var i = 0; i < myRoute.legs.length; i++) {
+        //ceo un marcador con esas propiedades por defecto
+        var marcador = markers[i] = markers[i] || new google.maps.Marker(
+                {
+                    title: 'Restaurante ' + (i + 1),
+                    icon: 'http://gmaps-samples.googlecode.com/svn/trunk/markers/orange/marker' + (i + 1) + '.png'
+                }
+            );
+
+        //creo el contenido del marcador
+        var texto = "<h3>Nombre: <small>" +
+            places[i].nombre +
+            "</small></h3><h3>Telefono: <small>" +
+            places[i].telefono +
+            "</small></h3><h3>Email: <small>" +
+            places[i].email +
+            "</small></h3><h3>Dirección: <small>" +
+            places[i].direccion +
+            "</small></h3>";
+        //asocio el marcador al mapa
+        marcador.setMap(map);
+        //añado el marcador a la posicion del mapa correspondiente
+        marcador.setPosition(myRoute.legs[i].start_location);
+
+        //le pongo el texto al marcador
+        attachInstructionText(stepDisplay, marcador, texto, map);
+
+    }
+
+}
+
+/**
+ * funcion que añade el texto personalizado al infowindow
+ * @param stepDisplay --infowindow
+ * @param marcador --punto de la ruta
+ * @param texto --texto personalizado
+ * @param map -- mapa donde se muestra la ruta
+ */
+function attachInstructionText(stepDisplay, marcador, texto, map) {
+
+    google.maps.event.addListener(marcador, 'click', function () {
+
+        //abro el infowindow cuando el marcador es clickado
+        stepDisplay.setContent(texto);
+        stepDisplay.open(map, marcador);
+    });
+
+}
+
+window.onload = initialize;
